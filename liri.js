7dd0225ed40c6cdbda9fe3a11ca20ad2b022965d
katjadulmaine <katja.dulmaine@gmail.com>
2019-04-24 02:21:20 -0400

    Files setup and npm installs and requires are done

---
+++ liri.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var spotify = new spotify(keys.spotify);
+
+
+var call = process.argv[2];
+
+// ??  .map method in JS
+switch (---){
+
+    case "concert-this": {
+
+        break;
+    }
+    case "spotify-this-song": {
+
+        break;
+    }
+    case "movie-this": { //if (!movieThis  "Mr. Nobody")
+
+        break;
+    }
+    case "do-what-it-says": {
+    
+        break;
+    }
+// append to log.txt
+};
+
+function concertThis() {
+
+}
